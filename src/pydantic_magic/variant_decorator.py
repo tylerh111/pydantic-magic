b98dee889f775e8377f8a97c08aa85d8ab3d7f62
Tyler Hughes <tylerhx111+git@proton.me>
2025-03-27 23:56:42 -0400

    added pydantic variant decorator

---
+++ src/pydantic_magic/variant_decorator.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+from typing import Any, Annotated, ClassVar, Union, get_args, get_origin
+
+from pydantic import RootModel
+
+
+__all__ = [
+    "pydantic_variant",
+]
+
+
+def pydantic_variant(
+    __cls = None,
+    /,
+    annotations: list | Any | None = None,
+) -> type | function:
+
+    annotations = annotations if annotations is not None else []
+    annotations = annotations if isinstance(annotations, list) else [annotations]
+
+    def inject(__cls):
+
+        def pydantic_variant_new(cls, *args, **kwargs):
+            # use root model to instantiate the variant (union or annotated union)
+            # alternatively, construct the super class of `cls` which will eventually hit the base class
+            # the recursive base case is when it reach the abstract base model
+            if cls is __cls:
+                return RootModel[__cls.model_variant](*args, **kwargs).root
+            return super(__cls, cls).__new__(cls, *args, **kwargs)
+
+        @classmethod
+        def pydantic_variant_init_subclass(cls, *args, **kwargs):
+
+            if get_origin(__cls.model_variant) is Annotated:
+                alternatives = get_args(__cls.model_variant)[0]
+                variant = Union[cls, alternatives]
+                __cls.model_variant = Annotated[(variant, *annotations)]
+            else:
+                alternatives = get_args(__cls.model_variant)
+                variant = Union[cls, alternatives]
+                __cls.model_variant = variant
+
+            return super(cls).__init_subclass__(*args, **kwargs)
+
+        __cls.__annotations__ = {"model_variant": ClassVar}
+        __cls.__new__ = pydantic_variant_new
+        __cls.__init_subclass__ = pydantic_variant_init_subclass
+
+        # note, this decorator cannot instantiate the base class, so it must be abstract
+        # this is due to need root model to validate the variant in the base case
+        # we purposefully leave out the base class in the variant to prevent this
+        # alternatives will only ever have subclasses of `__cls` (and `NoneType`)
+        if annotations:
+            __cls.model_variant = Annotated[(None, *annotations)]
+        else:
+            __cls.model_variant = None
+
+        return __cls
+
+    if __cls is None:
+        return inject
+
+    return inject(__cls)
